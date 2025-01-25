cf366dbead511ebcea5d56670798c784b2574359
joshiyash710 <joshiyash710@gmail.com>
2025-01-25 22:44:17 +0530

    Add initial backend files and configurations for nodemon and other modules

---
+++ Backend/index.js
@@ -0,0 +1,27 @@
+import express, { urlencoded } from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+import connectDB from './utils/db.js'
+dotenv.config()
+const app = express()
+app.get('/',(req,res)=>{
+    return res.status(200).json({
+        message : 'I am coming from backend !!!',
+        success : true
+    })
+})
+app.use(express.json())
+app.use(cookieParser())
+app.use(urlencoded({extended : true}))
+const corsOption = {
+    origin : 'http://localhost:5173',
+    credentials : true
+}
+app.use(cors(corsOption))
+const port = process.env.PORT || 3000
+app.listen(port,()=>{
+    connectDB()
+    console.log(`Server is listening at port ${port}`);
+    
+})
