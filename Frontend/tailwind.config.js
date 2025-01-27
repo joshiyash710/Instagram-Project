1b141751e0ff5e3ef676404e7c17ca6499919f82
joshiyash710 <joshiyash710@gmail.com>
2025-01-28 00:47:04 +0530

    Initialize frontend with React, Vite, and Tailwind CSS setup

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
+  
